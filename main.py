ff3ed18c34622cb07576213219510b88dbc72c02
johnm <j_mag_uidhir@outlook.ie>
2025-03-24 16:32:15 +0000

    App accessible on host machine

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+app.config['ENV'] = "Development"
+app.config['DEBUG'] = True
 
+@app.route('/')
+def search():
+    return render_template('search.html')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host="0.0.0.0", port=8888)

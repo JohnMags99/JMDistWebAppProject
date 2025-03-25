913434a38869463868e56a486150639cf913606d
johnm <j_mag_uidhir@outlook.ie>
2025-03-25 23:16:24 +0000

    /getResult now prints wiki article to output. Now needs to output as HTML at /getResult. IP needs to be updated after stopping the instance

--- searchWebApp.py
+++ searchWebApp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, flash
 from werkzeug.utils import redirect
+import paramiko
 
 app = Flask(__name__)
 app.config['ENV'] = "Development"
@@ -14,30 +15,36 @@ def searchHome():
 #Result Landing Page Function
 @app.route('/getResult', methods=['POST'])
 def getResult():
-    resultOutput = '''
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <title>Result</title>
-                <!--Bootstrap Framework CDN-->
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-            </head>
-            <body>
-                <div class="card">
-                    <div class="card-body">
-                        <h1 class="card-title">Results</h1>
-                        <h2 class="card-subtitle">I have no clue!</h2>
-                        <a href="/" class="btn btn-primary">Back to Search</a>
-                    </div>
-                </div>
-            </body>
-        </html>'''
-    return resultOutput
+    #Public IPv4 address on EC2 connect console, needs to be reviewed each time instance is stopped and started
+    instance_ip="51.20.12.44"
+    securityKeyFile="/home/johnm/DistWebAppProj.pem"
+    searchTerm=request.form.get('wSearch')
+    cmd="source wiki/wiki_env/bin/activate && python3 wiki.py"  #Activating venv before executing wiki.py
 
+    try:
+        #Attempt to connect/ssh to the instance
+        client=paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        key=paramiko.RSAKey.from_private_key_file(securityKeyFile)
+        client.connect(hostname=instance_ip, username="johnm", pkey=key)
+
+        #Executing wiki.py
+        stdin,stdout,stderr=client.exec_command(cmd+" "+searchTerm)
+        stdin.close()
+        outerr=stderr.readlines()
+        print("ERRORS: ", outerr)
+        output=stdout.readlines()
+
+        #TODO Output to HTML instead of console
+        #Get result
+        print("output: ",output)
+        for items in output:
+            print(items, end="")
+
+        #Close client connection when finished
+        client.close()
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
